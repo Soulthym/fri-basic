3fa09f6a8de1e3977cad947514afa3c764341a74
Thybault Alabarbe <thybault.alabarbe@gmail.com>
2024-08-29 01:58:41 +0200

    stark101: the basics - Fibonnaci Sq Trace

--- src/arithmetic.rs
+++ src/arithmetic.rs
@@ -11,6 +11,7 @@ trait Print {
     fn print_named(&self, name: &str);
     fn println_named(&self, name: &str);
 }
+
 #[derive(MontConfig)]
 #[modulus = "3221225473"]
 #[generator = "5"]
@@ -54,14 +55,46 @@ impl Print for Fq {
     }
 }
 
+type Int = BigInt<1>;
+
 #[cfg(test)]
 mod tests {
-
     use super::*;
+
     #[test]
     pub fn test_field() {
-        let g = Fq::from(FqConf::GENERATOR).pow::<BigInt<1>>(BigInt::<1>::new([3 * 2_u64.pow(20)]));
+        let g = Fq::from(FqConf::GENERATOR).pow::<Int>(Int::new([3 * 2_u64.pow(20)]));
         g.println_named("g");
         assert!(g.is_order(1024));
+    }
+
+    #[test]
+    pub fn stark_101_1() {
+        let mut a = vec![Fq::from(1), Fq::from(3141592)];
+        println!("{:?}", a);
+        for l in 2..1023 {
+            a.push(a[l - 2] * a[l - 2] + a[l - 1] * a[l - 1]);
+        }
+        println!("{:?}", a);
+        println!("{:?}", a.len());
+        assert_eq!(
+            a.len(),
+            1023,
+            "The trace must consist of exactly 1023 elements."
+        );
+        assert_eq!(
+            a[0],
+            Fq::from(1),
+            "The first element in the trace must be the unit element."
+        );
+        for i in 2..1023 {
+            assert_eq!(
+                a[i],
+                a[i - 1] * a[i - 1] + a[i - 2] * a[i - 2],
+                "The FibonacciSq recursion rule does not apply for index {i}"
+            );
+        }
+        assert_eq!(a[1022], Fq::from(2338775057_u64), "Wrong last element!");
+        println!("Success!");
     }
 }
