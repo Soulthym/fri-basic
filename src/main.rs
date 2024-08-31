bc5a432cd6c02d4abc7abf71ef7ca66df7a63a75
Thybault Alabarbe <thybault.alabarbe@gmail.com>
2024-08-31 16:28:56 +0200

    small test in main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 mod arithmetic;
 
 fn main() {
-    println!("arithmetic")
+    println!("arithmetic");
+    let u: u8 = 127;
+    let i: i8 = 127;
+    println!("u = {}", &u);
+    println!("i = {}", &i);
+    println!("u + 1 = {}", &u + 1);
+    println!("i + 1 = {}", i as u8 + 1);
+    let u: u8 = 0;
+    let i: i8 = 0;
+    println!("i = {}", &i);
+    println!("u = {}", &u);
+    println!("i - 1 = {}", i - 1);
+    println!("u - 1 = {}", u as i8 - 1);
 }
