2591cb17e385b7d3dbc53f059f2d5763ca3be259
Thybault Alabarbe <thybault.alabarbe@gmail.com>
2024-09-02 22:11:23 +0200

    test stark101 slide poly divisibility (constraints chapter)

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ type P = Poly<FE>;
 fn main() {
     let p = P::from(vec![1, 2, 3]);
     println!("{}", p);
-    let x = P::x();
-    println!("x = {}", x);
+    let top = P::from(vec![2, MODULUS - 3, 1]);
+    println!("top = {top}");
+    let bot = P::x() - P::from(2);
+    println!("bot = {bot}");
+    let (q, r) = top.divmod(bot.clone());
+    println!("{}\n/\n{}\n=\n{}\n(+ {})", top, bot, q, r);
 }
